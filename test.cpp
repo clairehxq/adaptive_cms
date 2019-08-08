f2e8e4e10b701860ba555a1a662afed8e332599a
clairehxq <xh895@nyu.edu>
2019-08-08 20:57:28 +0800

    t.c

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 //#include "tcs.hpp"
 //#include "cms.hpp"
 #include "adaptive_cms.hpp"
+#include "cms.hpp"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -9,25 +10,42 @@
 #include <set>
 #include <cstdio>
 
-
 int main(){
   printf("hello world\n");
   
   adaptive_cms acms(200, 5, 4, 3216, 0);
+  cms ccms(200, 5, 3216);
   printf("constructed\n");
   acms.check_config();
   
   std::set<uint32_t> hvalues;
-  for (uint64_t i=0; i<1531712; i++){
-    for (int j=0; j<5; j++){
-      hvalues.insert(acms.hash(i, j));
+  printf("add\n");
+  for (uint64_t i=0; i<1; i++){
+    for (uint32_t j=0; j<5; j++){
+      printf("%u th add\n", j*3);
+      acms.addDeep(i);
+      acms.addDeep(i);
+      acms.addDeep(i);
+      ccms.add(i);
+      ccms.add(i);
+      ccms.add(i);
+      printf("\n");
     }
   }
-
-  for (auto& i : hvalues){
-    printf("%u ", i);
+  
+  printf("countmindeep\n");
+  for(uint64_t i=0; i<1; i++){
+    for(uint32_t j=0; j<5; j++) {
+      uint32_t hvalue = acms.hash(i,j);
+      printf("%u hashed to %u ", j, hvalue);
+    }
+    //printf("%d estimate count %ul\n", i, acms.countMinDeep(i));
+    printf("%u estimate count %u\n", i, acms.countMinDeep(i));
+    printf("%u ccms estimate count %u\n", i, ccms.countMin(i));
   }
-  printf("length %d\n", hvalues.size());
+
+  acms.del();
+  ccms.del();
   return 1;
 }
 
